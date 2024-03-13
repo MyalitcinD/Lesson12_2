9bc7b3a8684911049c0c6e01ee8020fe9a5a42b2
Денис <mden1996.07@gmail.com>
2024-03-13 03:24:54 +0300

    Добавьте файлы проекта.

---
+++ Task/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    static class Circle
+    {
+
+        static public double GetCircumference(double radius)
+        {
+            return Math.PI * 2 * radius;
+        }
+
+        static public double GetArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+        static public bool CordCheck(double x, double y, double radius, double x0, double y0)
+        {
+
+            //   ((x-x0)*(x-x0)+(y-y0)*(y-y0))<=(radius*radius)
+            if((((x - x0) * (x - x0) + (y - y0) * (y - y0)) <= (radius * radius))) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+
+
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x,y проверяемой точки");
+            Console.Write("x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите радиус R и координаты x0 yo центра окружности");
+            Console.Write("R = ");
+            double R = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x0 = ");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y0 = ");
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Длина окружности = {0}", Circle.GetCircumference(R));
+            Console.WriteLine("Площадь круга = {0}", Circle.GetArea(R));
+            if(Circle.CordCheck(x, y, R, x0, y0)) {
+                Console.WriteLine("Точка принадлежит кругу");
+            }
+            else {
+                Console.WriteLine("Точка НЕ принадлежит кругу");
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
